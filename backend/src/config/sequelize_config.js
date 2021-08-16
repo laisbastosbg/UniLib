6fb24442f94761e50c1886c3011079746cfb65bf
Lais Godinho <lais2bg@gmail.com>
2021-08-16 10:55:57 -0300

    inicializaçao dos projetos node e react

---
+++ backend/src/config/sequelize_config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  port: "54320",
+  username: "postgres",
+  password: "postgres",
+  database: "postgres",
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+};
---
+++ backend/src/database/migrations/20210816134603-create-users.js
@@ -0,0 +1,72 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable("users", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      login: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      cpf: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      phone_number: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      birthdate: {
+        type: Sequelize.DATE,
+        allowNUll: true,
+      },
+      admission_date: {
+        type: Sequelize.DATE,
+        allowNUll: true,
+      },
+      profile: {
+        type: Sequelize.STRING,
+        allowNull: false, 
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNUll: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNUll: false,
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNUll: false,
+      },
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("users");
+  }
+};
